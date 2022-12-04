b5d72071a0e701f7d7f83d05794c16182a614f58
blucheez <petreistvan94@gmail.com>
2022-12-04 12:15:44 +0100

    cleaned up jsx, moved function to utils folder

--- js-numerals/src/components/Converter.js
+++ js-numerals/src/components/Converter.js
@@ -1,5 +1,5 @@
-import { constants } from '../utils/constants'
 import { useState } from 'react'
+import { convertToString } from '../utils/converterFunc'
 
 function Converter() {
   const [number, setNumber] = useState('')
@@ -10,57 +10,6 @@ function Converter() {
     } else {
       setNumber('')
     }
-  }
-
-  const convertToString = (inputNumber) => {
-    let string = ''
-    let smallerThanInputValue = constants.one
-    let smallerThanInputKey = 'one'
-    let remainder = inputNumber
-    for (let [key, value] of Object.entries(constants)) {
-      if (inputNumber === value && inputNumber < 100) {
-        return key
-      }
-      if (inputNumber === value && inputNumber >= 100) {
-        return `one ${key}`
-      }
-      if (value < inputNumber) {
-        smallerThanInputValue = value
-        smallerThanInputKey = key
-        remainder = getRemainder(inputNumber, smallerThanInputValue)
-      }
-      if (value > inputNumber) break
-    }
-    if (inputNumber > 20 && inputNumber < 100) {
-      const starter = smallerThanInputKey
-      const convRemainder = convertToString(remainder)
-      string = `${starter}-${convRemainder}`
-    }
-    if (inputNumber >= 100) {
-      const firstNum = convertToString(
-        Math.floor(inputNumber / smallerThanInputValue)
-      )
-      const starter = smallerThanInputKey
-      const convRemainder = convertToString(remainder)
-      if (!remainder) {
-        string = `${firstNum} ${starter}`
-      } else if (
-        remainder > 0 &&
-        smallerThanInputValue >= 1000 &&
-        remainder < 100
-      ) {
-        string = `${firstNum} ${starter} and ${convRemainder}`
-      } else if (remainder > 0 && smallerThanInputValue === 100) {
-        string = `${firstNum} ${starter} and ${convRemainder}`
-      } else {
-        string = `${firstNum} ${starter} ${convRemainder}`
-      }
-    }
-    return string
-  }
-
-  const getRemainder = (inputNumber, smaller) => {
-    return inputNumber - Math.floor(inputNumber / smaller) * smaller
   }
 
   return (
