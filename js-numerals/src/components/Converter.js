721949edf10b474903b4ca6b79ee9cae09a68715
blucheez <petreistvan94@gmail.com>
2022-12-04 12:12:19 +0100

    added support for higher number, added styles for the page

--- js-numerals/src/components/Converter.js
+++ js-numerals/src/components/Converter.js
@@ -36,7 +36,26 @@ function Converter() {
       const convRemainder = convertToString(remainder)
       string = `${starter}-${convRemainder}`
     }
-
+    if (inputNumber >= 100) {
+      const firstNum = convertToString(
+        Math.floor(inputNumber / smallerThanInputValue)
+      )
+      const starter = smallerThanInputKey
+      const convRemainder = convertToString(remainder)
+      if (!remainder) {
+        string = `${firstNum} ${starter}`
+      } else if (
+        remainder > 0 &&
+        smallerThanInputValue >= 1000 &&
+        remainder < 100
+      ) {
+        string = `${firstNum} ${starter} and ${convRemainder}`
+      } else if (remainder > 0 && smallerThanInputValue === 100) {
+        string = `${firstNum} ${starter} and ${convRemainder}`
+      } else {
+        string = `${firstNum} ${starter} ${convRemainder}`
+      }
+    }
     return string
   }
 
@@ -45,20 +64,32 @@ function Converter() {
   }
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center vh-100'>
-      <form className='d-flex flex-column'>
-        <label htmlFor='number' className='form-label mb-2 display-6'>
-          Number
-        </label>
-        <input
-          type='number'
-          name='number'
-          id='number'
-          className='form-control-lg'
-          onChange={handleInput}
-        />
-      </form>
-      <div className='mt-5'>{convertToString(number)}</div>
+    <div className='container'>
+      <div className='d-flex flex-column justify-content-center align-items-center vh-100 text-center'>
+        <div className='display-4'>Arabic number conversion tool</div>
+        <div className='fs-5'>
+          This tool converts numeric input into the English phrase of that
+          number.
+        </div>
+        <div>eg. 42 - forty-two, 2001 - two thousand and one</div>
+        <form className='d-flex flex-column mt-5 align-items-center'>
+          <label htmlFor='number' className='form-label mb-2 display-6'>
+            Number
+          </label>
+          <input
+            type='number'
+            name='number'
+            id='number'
+            className='form-control-lg'
+            onChange={handleInput}
+            placeholder='Start typing...'
+          />
+        </form>
+        <div className='mt-5 display-6 d-flex flex-column align-items-center '>
+          Result
+          <div className='text-warning'>{convertToString(number)}</div>
+        </div>
+      </div>
     </div>
   )
 }
